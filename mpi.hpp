1c8851066e327749197f0685c8cc45fe1a42a7cb
Yi Zhang <yiz@metrumrg.com>
2020-10-19 21:46:46 -0700

    move cross_chain unit tests into torsten

--- mpi.hpp
+++ mpi.hpp
@@ -2,7 +2,6 @@
 #define STAN_MATH_TORSTEN_MPI_HPP
 
 #include <stan/math/torsten/mpi/my_worker.hpp>
-#include <stan/math/torsten/mpi/session.hpp>
 #include <stan/math/torsten/mpi/environment.hpp>
 #include <stan/math/torsten/mpi/session.hpp>
 #include <stan/math/torsten/mpi/session_def.hpp>
