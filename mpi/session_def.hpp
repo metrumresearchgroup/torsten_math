12f6b3170bdd847235fb244eaefb202ecab2a17f
Yi Zhang <yiz@metrumrg.com>
2020-11-11 22:44:03 -0800

    fix mpi initailzation error for non-mpi compilation

--- mpi/session_def.hpp
+++ mpi/session_def.hpp
@@ -5,6 +5,7 @@
  * Define static variables of <code>Session</code> \
  * Only to be included in tests & cmdstan top level #<code>command.hpp</code> \
  */
+#if defined(STAN_LANG_MPI) || defined(TORSTEN_MPI)
 #define TORSTEN_MPI_SESSION_INIT \
  stan::math::mpi::Envionment   stan::math::mpi::Session::env; \
                       MPI_Comm stan::math::mpi::Session::MPI_COMM_INTER_CHAIN(MPI_COMM_NULL); \
@@ -12,5 +13,8 @@
  stan::math::mpi::Communicator stan::math::mpi::Session::inter_chain(MPI_COMM_NULL); \
  stan::math::mpi::Communicator stan::math::mpi::Session::intra_chain(MPI_COMM_NULL); \
  int torsten::mpi::Session::num_chains = 1;
+#else
+#define TORSTEN_MPI_SESSION_INIT
+#endif
 
 #endif
